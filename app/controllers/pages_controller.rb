218d17ddc72f7610028bbaaf6e892f60c8de00c3
ethornbury <thornburye@gmail.com>
2022-11-23 15:04:48 +0000

    trying to set up dotenv vars

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
+
 class PagesController < ApplicationController
+  
   def home
     response = HTTParty.get("https://api.publicapis.org/categories")
-    console #to open a space onscreen at localhost to view the response
+    #console    #to open a space onscreen at localhost to view the response
     
     #in the console, bottom of the html page in localhost, type 'response'
     #reponse.body
@@ -14,5 +16,13 @@ class PagesController < ApplicationController
     #another way
     @cat_facts = JSON.parse(HTTParty.get("https://cataas.com/api/tags").body)
     
+    # https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=yourkey
+
+    response2 = HTTParty.get("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=NYT_API")
+    @nyt_data = JSON.parse(response2.body)
+
+    
+    
+
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,17 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# Load dotenv only in development or test environment
+if ['development', 'test'].include? ENV['NYT_API']
+  Dotenv::Railtie.load
+end
+
+NYT_API = ENV['NYT_API']
 
 module Video
   class Application < Rails::Application
